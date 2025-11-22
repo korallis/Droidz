6a68c4869393ec01037efa3050701a47bfc02568
korallis <lee.barry84@gmail.com>
2025-11-22 01:50:37 +0000

    chore: update installer version to v3.1.0

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DROIDZ_VERSION="2.7.6"
+DROIDZ_VERSION="3.1.0"
 GITHUB_RAW="https://raw.githubusercontent.com/korallis/Droidz/main"
 CACHE_BUST="?v=${DROIDZ_VERSION}&t=$(date +%s)"
 
